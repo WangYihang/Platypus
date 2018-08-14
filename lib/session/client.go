6e26245d4d7640f56038b31e6effb283f4853af2
Wang Yihang <yihangwang@tencent.com>
2018-08-14 21:50:46 +0800

    [+] Add help menu

---
+++ lib/session/client.go
@@ -0,0 +1,43 @@
+package session
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"time"
+)
+
+type Client struct {
+	ts          time.Time
+	conn        net.Conn
+	interactive bool
+}
+
+func CreateClient(conn net.Conn) *Client {
+	return &Client{
+		ts:          time.Now(),
+		conn:        conn,
+		interactive: false,
+	}
+}
+
+func (c Client) Hash() string {
+	return MD5(c.Desc())
+}
+
+func (c Client) Close() {
+	delete(server.clients, c.Hash())
+	c.conn.Close()
+}
+
+func (c Client) Desc() string {
+	addr := c.conn.RemoteAddr()
+	return fmt.Sprintf("%s://%s", addr.Network(), addr.String())
+}
+
+func MD5(data string) string {
+	m := md5.New()
+	m.Write([]byte(data))
+	return hex.EncodeToString(m.Sum(nil))
+}
