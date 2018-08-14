bf9cf8a77abd61c3defe9b44e7f4c932b89cd305
Wang Yihang <yihangwang@tencent.com>
2018-08-15 02:34:31 +0800

    [+] Implementation of Executing meta command

---
+++ lib/cli/dispatcher/jump.go
@@ -0,0 +1,36 @@
+package dispatcher
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/WangYihang/Platypus/lib/util/log"
+)
+
+func (ctx Dispatcher) Jump(args []string) {
+	if len(args) != 1 {
+		log.Error("Argments error, use `Help Jump` to get more Jumprmation")
+		ctx.JumpHelp([]string{})
+		return
+	}
+	for _, server := range Servers {
+		for _, client := range server.Clients {
+			if strings.HasPrefix(client.Hash, strings.ToLower(args[0])) {
+				Current = client
+				return
+			}
+		}
+	}
+	log.Error("No such node")
+}
+
+func (ctx Dispatcher) JumpHelp(args []string) {
+	fmt.Println("Usage of Jump")
+	fmt.Println("\tJump [HASH]")
+	fmt.Println("\tHASH\tThe hash of an node, node can be both a server or a client")
+}
+
+func (ctx Dispatcher) JumpDesc(args []string) {
+	fmt.Println("Jump")
+	fmt.Println("\tThis command will jump to a node, waiting to interactive with it")
+}
