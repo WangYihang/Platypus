2ebf38c5ac2c1b9a1cc79eb111e1fa853eec7265
Wang Yihang <yihangwang@tencent.com>
2018-08-14 18:43:37 +0800

    [+] Seperate to different folders

--- platypus.go
+++ platypus.go
@@ -1,81 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"time"
+	"github.com/WangYihang/Platypus/lib/cli"
 )
 
-type client struct {
-	addr net.Addr
-	ts   time.Time
-}
-
-var clients map[string]client
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
-	}
-}
-
-func handleClient(conn net.Conn) {
-	defer conn.Close()
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime))
-}
-
-func startServer(host string, port int16) {
-	service := fmt.Sprintf("%s:%d", host, port)
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	fmt.Println("Server running at: ", service)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		go handleClient(conn)
-	}
-}
-
-func commandDispatcher() {
-	inputReader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println("Please enter some input: ")
-		input, err := inputReader.ReadString('\n')
-		if err == nil {
-			fmt.Printf("The input was: %s\n", input)
-		}
-		exitFlag := false
-		switch input {
-		case "exit\n":
-			fmt.Println("exiting...")
-			exitFlag = true
-			break
-		case "run\n":
-			go startServer("0.0.0.0", 4444)
-			break
-		default:
-			fmt.Println("Unsupported command: ", input)
-		}
-		if exitFlag {
-			break
-		}
-	}
-}
-
 func main() {
-	commandDispatcher()
+	cli.CommandDispatcher()
 }
