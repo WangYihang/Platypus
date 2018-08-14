e4ad91535587de04b21a4bbf1494d6659bcddc1a
Wang Yihang <yihangwang@tencent.com>
2018-08-14 21:50:46 +0800

    [+] Add help menu

--- lib/cli/dispatcher.go
+++ lib/cli/dispatcher.go
@@ -4,33 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
-	"github.com/WangYihang/Platypus/lib/server"
+	"github.com/WangYihang/Platypus/lib/session"
 )
 
 var commands = map[string]string{
-	"exit": "Exit context",
+	"exit":     "Exit context",
+	"sessions": "List the connected sessions",
 }
 
+var servers []session.Server
+
+func Help() {
+	fmt.Println("Usage: ")
+	for k, v := range commands {
+		fmt.Println("\t", k, "\t", v)
+	}
+}
+
+// CommandDispatcher dsa
+//
 func CommandDispatcher() {
 	inputReader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("Please enter some input: ")
+		fmt.Print(">> ")
 		input, err := inputReader.ReadString('\n')
-		if err == nil {
-			fmt.Printf("The input was: %s\n", input)
+		if err != nil {
+			fmt.Println("Read from stdin failed")
 		}
+		input = strings.TrimSpace(input)
 		exitFlag := false
 		switch input {
-		case "exit\n":
-			fmt.Println("exiting...")
+		case "exit":
+			for _, server := range servers {
+				server.Cleanup()
+			}
 			exitFlag = true
 			break
-		case "run\n":
-			go server.Start("0.0.0.0", 4444)
+		case "list":
+			for _, server := range servers {
+				fmt.Println(server.Desc())
+			}
+		case "run":
+			s := session.CreateServer("0.0.0.0", 4444)
+			servers = append(servers, *s)
+			go s.Run()
 			break
 		default:
-			fmt.Println("Unsupported command: ", input)
+			Help()
 		}
 		if exitFlag {
 			break
---
+++ lib/session/server.go
@@ -0,0 +1,95 @@
+package session
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+type Server struct {
+	host    string
+	port    int16
+	clients map[string](*Client)
+	ts      time.Time
+}
+
+var server *Server
+
+func CreateServer(host string, port int16) *Server {
+	return &Server{
+		host:    host,
+		port:    port,
+		clients: make(map[string](*Client)),
+		ts:      time.Now(),
+	}
+}
+
+func (s Server) Run() {
+	service := fmt.Sprintf("%s:%d", s.host, s.port)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	fmt.Println("Server running at: ", service)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		client := CreateClient(conn)
+		fmt.Println(client.Desc())
+		go s.AddClient(client)
+	}
+}
+
+func (s Server) Desc() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(
+		fmt.Sprintf(
+			"[%s] %s:%d (%d online clients)",
+			s.ts.Format("2006-01-02 15:04:05"),
+			s.host,
+			s.port,
+			len(s.clients),
+		),
+	)
+	var descs []string
+	for hash, client := range s.clients {
+		descs = append(descs, fmt.Sprintf("\t%s (%s)", client.Desc(), hash))
+	}
+	if len(descs) > 0 {
+		buffer.WriteString("\n")
+	}
+	buffer.WriteString(strings.Join(descs, "\n"))
+	return buffer.String()
+}
+
+func (s Server) Stop() {
+	// TODO
+}
+
+func (s Server) AddClient(client *Client) {
+	s.clients[client.Hash()] = client
+}
+
+func (s Server) DeleteClient(client Client) {
+	delete(s.clients, client.Desc())
+}
+
+func (s Server) GetAllClients() map[string](*Client) {
+	return s.clients
+}
+
+func (s Server) Cleanup() {
+	for _, client := range s.clients {
+		client.conn.Close()
+	}
+}
