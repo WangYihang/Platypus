7ff0c6d9a5a87fe3d393f49ed5a45df6972cc9b8
Wang Yihang <yihangwang@tencent.com>
2018-08-14 17:13:57 +0800

    [+] Bare socket server

--- platypus.go
+++ platypus.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	daytime := time.Now().String()
+	conn.Write([]byte(daytime))
+}
+
+func main() {
+	service := ":4444"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
 }
