69cfa8f7e2b82442e036016657d886e50ffe50f2
Wang Yihang <yihangwang@tencent.com>
2018-08-15 19:47:29 +0800

    [+] Fix undeleted bug

--- lib/context/context.go
+++ lib/context/context.go
@@ -1,7 +1,10 @@
 package context
 
 import (
+	"net"
+
 	"github.com/WangYihang/Platypus/lib/model"
+	"github.com/WangYihang/Platypus/lib/util/log"
 )
 
 type Context struct {
@@ -28,5 +31,55 @@ func (ctx Context) DeleteClient(c *model.Client) {
 	}
 	if c == ctx.Current {
 		ctx.Current = nil
+	}
+}
+
+func (ctx Context) RunServer(server *model.Server, listener *net.TCPListener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		client := model.CreateClient(conn)
+		log.Info("New client %s Connected", client.Desc())
+		server.AddClient(client)
+		go Read(client, ctx)
+		go Write(client, ctx)
+	}
+}
+
+func Read(c *model.Client, ctx Context) {
+	for {
+		buffer := make([]byte, 1024)
+		_, err := c.Conn.Read(buffer)
+		if err != nil {
+			log.Error("Read failed from %s , error message: %s", c.Desc(), err)
+			close(c.OutPipe)
+			ctx.DeleteClient(c)
+			return
+		}
+		c.OutPipe <- buffer
+	}
+}
+
+func Write(c *model.Client, ctx Context) {
+	for {
+		select {
+		case data, ok := <-c.InPipe:
+			if !ok {
+				log.Error("Channel of %s closed", c.Desc())
+				close(c.InPipe)
+				ctx.DeleteClient(c)
+				return
+			}
+			n, err := c.Conn.Write(data)
+			if err != nil {
+				log.Error("Write failed to %s , error message: %s", c.Desc(), err)
+				close(c.InPipe)
+				ctx.DeleteClient(c)
+				return
+			}
+			log.Info("%d bytes sent", n)
+		}
 	}
 }
