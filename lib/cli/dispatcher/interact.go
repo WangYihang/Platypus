b0a979cad0efdbe411e3bb0ac87176a62a13a775
Wang Yihang <yihangwang@tencent.com>
2018-08-15 16:03:48 +0800

    [+] Implementation EOF error detection

--- lib/cli/dispatcher/interact.go
+++ lib/cli/dispatcher/interact.go
@@ -14,17 +14,51 @@ func (ctx Dispatcher) Interact(args []string) {
 		return
 	}
 	log.Info("Interacting with %s", Current.Desc())
+	// write to socket fd
 	inputChannel := make(chan []byte, 1024)
 	go func() {
 		for {
 			select {
 			case data := <-inputChannel:
+				if Current == nil {
+					return
+				}
 				Current.Conn.Write(data)
+			}
+		}
+	}()
+	// read from socket fd
+	go func() {
+		for {
+			buffer := make([]byte, 1024)
+			if Current == nil {
+				return
+			}
+			n, err := Current.Conn.Read(buffer)
+			if err != nil {
+				log.Error("Read failed from %s , error message: %s", Current.Desc(), err)
+				// Clean up
+				for _, server := range Servers {
+					for _, client := range server.Clients {
+						if client.Hash == Current.Hash {
+							server.DeleteClient(client)
+						}
+					}
+				}
+				// Set Current to nil
+				Current = nil
+				break
+			}
+			if Current.Interactive {
+				fmt.Print(string(buffer[:n]))
 			}
 		}
 	}()
 
 	for {
+		if Current == nil {
+			return
+		}
 		inputReader := bufio.NewReader(os.Stdin)
 		command, err := inputReader.ReadString('\n')
 		if err != nil {
@@ -37,7 +71,6 @@ func (ctx Dispatcher) Interact(args []string) {
 		}
 		inputChannel <- []byte(command)
 	}
-	log.Info("Detach from %s", Current.Desc())
 }
 
 func (ctx Dispatcher) InteractHelp(args []string) {
