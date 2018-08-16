498da07a562d381a47de2d16f7d00bf00601c2cb
Wang Yihang <yihangwang@tencent.com>
2018-08-16 16:50:28 +0800

    [+] Import cycle solved

---
+++ lib/model/context.go
@@ -0,0 +1,52 @@
+package model
+
+import (
+	"net"
+
+	"github.com/WangYihang/Platypus/lib/util/log"
+)
+
+type Context struct {
+	Servers       map[string](*Server)
+	Current       *Client
+	CommandPrompt string
+}
+
+var Ctx *Context
+
+func InitContext() {
+	if Ctx == nil {
+		Ctx = &Context{
+			Servers:       make(map[string](*Server)),
+			Current:       nil,
+			CommandPrompt: ">> ",
+		}
+	}
+}
+
+func GetContext() *Context {
+	return Ctx
+}
+
+func (ctx Context) DeleteClient(c *Client) {
+	for _, server := range Ctx.Servers {
+		server.DeleteClient(c)
+	}
+	// if c == Ctx.Current {
+	// 	Ctx.Current = nil
+	// }
+}
+
+func (ctx Context) RunServer(server *Server, listener *net.TCPListener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		client := CreateClient(conn)
+		log.Info("New client %s Connected", client.Desc())
+		server.AddClient(client)
+		go client.Read()
+		go client.Write()
+	}
+}
