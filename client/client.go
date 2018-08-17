2935268be8fbc0ef0641ab01370ca1aa4772526e
Wang Yihang <yihangwang@tencent.com>
2018-08-17 22:19:13 +0800

    [+] Upgrade client using message

--- client/client.go
+++ client/client.go
@@ -9,27 +9,41 @@ import (
 	"github.com/WangYihang/Platypus/lib/util/log"
 )
 
+var Key []byte
+
+func init() {
+	Key = []byte("VnwkyMTUgmzVxUi6")
+}
+
+func HandleMessage(conn net.Conn, message platypus.Message) {
+	switch message.Type {
+	case platypus.AUTH:
+		token, err := crypto.Decrypt(Key, message.Content)
+		if err != nil {
+			log.Error("Decrypt challenge failed, %s", err)
+		}
+		log.Success("Token: %s", token)
+		conn.Write(token)
+	}
+}
+
 func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:4444")
 	if err != nil {
 		log.Error("Connection failed, %s", err)
 		return
 	}
-	key := []byte("VnwkyMTUgmzVxUi6")
 
-	// Decode message
-	dec := gob.NewDecoder(conn)
-	var message platypus.Message
-	err = dec.Decode(&message)
-	if err != nil {
-		log.Error("decode error: %s", err)
+	for {
+		dec := gob.NewDecoder(conn)
+		var message platypus.Message
+		err = dec.Decode(&message)
+		if err != nil {
+			log.Error("decode error: %s", err)
+			continue
+		}
+		HandleMessage(conn, message)
 	}
 
-	token, err := crypto.Decrypt(key, message.Content)
-	if err != nil {
-		log.Error("Decrypt challenge failed, %s", err)
-	}
-	log.Success("Token: %s", token)
-	conn.Write(token)
 	conn.Close()
 }
