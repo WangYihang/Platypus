1d5e804aa95fa48d804b7e2ce1bcda913920e539
Wang Yihang <yihangwang@tencent.com>
2018-08-17 16:34:26 +0800

    [+] Create platypus Client auth method arch

--- lib/model/platypus/server.go
+++ lib/model/platypus/server.go
@@ -2,26 +2,72 @@ package platypus
 
 import (
 	"fmt"
+	"net"
 	"time"
 
 	"github.com/WangYihang/Platypus/lib/context"
 	"github.com/WangYihang/Platypus/lib/util/hash"
+	"github.com/WangYihang/Platypus/lib/util/log"
 )
 
-type PlatypusTCPServer struct {
+type PlatypusServer struct {
 	context.TCPServer
 }
 
-func CreatePlatypusTCPServer(host string, port int16) *PlatypusTCPServer {
+func CreatePlatypusServer(host string, port int16) *PlatypusServer {
 	ts := time.Now()
-	return &PlatypusTCPServer{
+	return &PlatypusServer{
 		context.TCPServer{
-			Name:      "Reverse",
+			Name:      "Platypus",
 			Host:      host,
 			Port:      port,
-			Clients:   make(map[string](*context.Client)),
+			Clients:   make(map[string](*context.TCPClient)),
 			TimeStamp: ts,
 			Hash:      hash.MD5(fmt.Sprintf("%s:%s:%s", host, port, ts)),
 		},
 	}
+}
+
+func (s *PlatypusServer) Run() {
+	log.Info("Platypus server running...")
+	service := fmt.Sprintf("%s:%d", s.Host, s.Port)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	if err != nil {
+		log.Error("Resolve TCP address failed: ", err)
+		return
+	}
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Error("Listen failed: ", err)
+		return
+	}
+	log.Info(fmt.Sprintf("Server running at: %s", s.FullDesc()))
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		client := CreatePlatypusClient(conn)
+		log.Info("New client %s Connected", client.Desc())
+		if client.Auth() {
+			s.AddPlatypusClient(&client.TCPClient)
+		} else {
+			log.Warn("Client %s auth failed", client.Desc())
+			client.Close()
+		}
+	}
+}
+
+func (s *PlatypusServer) AddPlatypusClient(client *context.TCPClient) {
+	s.Clients[client.Hash] = client
+}
+
+func (s *PlatypusServer) DeletePlatypusClient(client *context.TCPClient) {
+	client.Close()
+	delete(s.Clients, client.Hash)
+}
+
+func (s *PlatypusServer) GetAllPlatypusClients() map[string](*context.TCPClient) {
+	return s.Clients
 }
