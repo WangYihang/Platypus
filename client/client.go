362b846fd82fe3bc1929e702beda3e52660b6f15
Wang Yihang <yihangwang@tencent.com>
2018-08-17 20:41:17 +0800

    [+] Add client of platypus

---
+++ client/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+
+	"github.com/WangYihang/Platypus/lib/util/crypto"
+	"github.com/WangYihang/Platypus/lib/util/log"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:4444")
+	if err != nil {
+		log.Error("Connection failed, %s", err)
+		return
+	}
+	key := []byte("VnwkyMTUgmzVxUi6")
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Error("Read from server failed, %s", err)
+	}
+	log.Success("%d bytes read from server", string(n))
+	token, err := crypto.Decrypt(key, buffer[:n])
+	if err != nil {
+		log.Error("Decrypt challenge failed, %s", err)
+	}
+	log.Success("Token: %s", token)
+	conn.Write(token)
+	conn.Close()
+}
