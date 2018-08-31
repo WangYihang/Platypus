051ab20ddf0b58e629cc4a987d7446a3fce31c2c
Wang Yihang <wangyihanger@gmail.com>
2018-08-31 11:38:47 +0800

    [+] Modify log lib, works on both Win/*nix

--- lib/util/log/log.go
+++ lib/util/log/log.go
@@ -3,16 +3,11 @@ package log
 import (
 	"fmt"
 	"time"
+	"github.com/fatih/color"
 )
 
 const (
-	colorRed = uint8(iota + 91)
-	colorGreen
-	colorYellow
-	colorBlue
-	colorMagenta
-
-	debug   = "[TRAC]"
+	debug   = "[DEBUG]"
 	info    = "[INFO]"
 	err     = "[ERROR]"
 	warn    = "[WARN]"
@@ -20,53 +15,38 @@ const (
 )
 
 func Debug(format string, a ...interface{}) {
-	prefix := yellow(debug)
-	fmt.Println(formatLog(prefix), fmt.Sprintf(format, a...))
+	color.New(color.FgYellow).Printf(debug + " ")
+	color.New(color.FgHiBlack).Printf(formatTime() + " ")
+	fmt.Println(fmt.Sprintf(format, a...))
 }
 
 func Info(format string, a ...interface{}) {
-	prefix := blue(info)
-	fmt.Println(formatLog(prefix), fmt.Sprintf(format, a...))
+	color.New(color.FgBlue).Printf(info + " ")
+	color.New(color.FgHiBlack).Printf(formatTime() + " ")
+	fmt.Println(fmt.Sprintf(format, a...))
 }
 
 func Error(format string, a ...interface{}) {
-	prefix := red(err)
-	fmt.Println(formatLog(prefix), fmt.Sprintf(format, a...))
+	color.New(color.FgRed).Printf(err + " ")
+	color.New(color.FgHiBlack).Printf(formatTime() + " ")
+	fmt.Println(fmt.Sprintf(format, a...))
 }
 func Warn(format string, a ...interface{}) {
-	prefix := magenta(warn)
-	fmt.Println(formatLog(prefix), fmt.Sprintf(format, a...))
+	color.New(color.FgMagenta).Printf(warn + " ")
+	color.New(color.FgHiBlack).Printf(formatTime() + " ")
+	fmt.Println(fmt.Sprintf(format, a...))
 }
 
 func Success(format string, a ...interface{}) {
-	prefix := green(success)
-	fmt.Println(formatLog(prefix), fmt.Sprintf(format, a...))
+	color.New(color.FgGreen).Printf(success + " ")
+	color.New(color.FgHiBlack).Printf(formatTime() + " ")
+	fmt.Println(fmt.Sprintf(format, a...))
 }
 
 func CommandPrompt(commandPrompt string) {
-	fmt.Print(blue(commandPrompt))
+	color.New(color.FgYellow).Print(commandPrompt)
 }
 
-func red(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorRed, s)
-}
-
-func green(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorGreen, s)
-}
-
-func yellow(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorYellow, s)
-}
-
-func blue(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorBlue, s)
-}
-
-func magenta(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorMagenta, s)
-}
-
-func formatLog(prefix string) string {
-	return time.Now().Format("2006/01/02 15:04:05") + " " + prefix + " "
+func formatTime() string {
+	return time.Now().Format("2006/01/02 15:04:05")
 }
