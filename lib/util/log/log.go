f971288c391a72f771539209791d9166356d010d
Wang Yihang <wangyihanger@gmail.com>
2018-08-31 13:48:26 +0800

    [+] fix log message

--- lib/util/log/log.go
+++ lib/util/log/log.go
@@ -15,32 +15,32 @@ const (
 	success = "[SUCCESS]"
 )
 
-func printMessagePrefix(colorNumber color.Attribute) {
-	color.New(colorNumber).Printf(debug + " ")
+func printMessagePrefix(colorNumber color.Attribute, message string) {
+	color.New(colorNumber).Printf(message + " ")
 	color.New(color.FgHiBlack).Printf(formatTime() + " ")
 }
 
 func Debug(format string, a ...interface{}) {
-	printMessagePrefix(color.FgYellow)
+	printMessagePrefix(color.FgYellow, debug)
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, a...))
 }
 
 func Info(format string, a ...interface{}) {
-	printMessagePrefix(color.FgBlue)
+	printMessagePrefix(color.FgBlue, info)
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, a...))
 }
 
 func Error(format string, a ...interface{}) {
-	printMessagePrefix(color.FgRed)
+	printMessagePrefix(color.FgRed, err)
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, a...))
 }
 func Warn(format string, a ...interface{}) {
-	printMessagePrefix(color.FgMagenta)
+	printMessagePrefix(color.FgMagenta, warn)
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, a...))
 }
 
 func Success(format string, a ...interface{}) {
-	printMessagePrefix(color.FgGreen)
+	printMessagePrefix(color.FgGreen, success)
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, a...))
 }
 
