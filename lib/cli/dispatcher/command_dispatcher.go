2e60056aab2ab73699a09e85a38c00e1fbf6be20
Wang Yihang <wangyihanger@gmail.com>
2018-08-31 13:23:38 +0800

    [+] use readline, support *nix/win, support history

--- lib/cli/dispatcher/command_dispatcher.go
+++ lib/cli/dispatcher/command_dispatcher.go
@@ -1,20 +1,19 @@
 package dispatcher
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"io"
 	"strings"
 
-	"github.com/WangYihang/Platypus/lib/context"
-	"github.com/WangYihang/Platypus/lib/util/log"
+	"github.com/chzyer/readline"
+	"github.com/WangYihang/Platypus/lib/util/log" 
+	"github.com/WangYihang/Platypus/lib/context" 
 	"github.com/WangYihang/Platypus/lib/util/reflection"
 	"github.com/WangYihang/Platypus/lib/util/str"
 )
 
 type Dispatcher struct{}
 
-func ParseInput(input string) (string, []string) {
+func parseInput(input string) (string, []string) {
 	methods := reflection.GetAllMethods(Dispatcher{})
 	args := strings.Split(strings.TrimSpace(input), " ")
 	if !reflection.Contains(methods, args[0]) {
@@ -24,18 +23,57 @@ func ParseInput(input string) (string, []string) {
 	return str.UpperCaseFirstChar(args[0]), args[1:]
 }
 
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
+}
+
 func Run() {
-	inputReader := bufio.NewReader(os.Stdin)
-	reflection.Invoke(Dispatcher{}, "Help", []string{})
-	for {
-		log.CommandPrompt(context.Ctx.CommandPrompt)
-		input, err := inputReader.ReadString('\n')
-		if err != nil {
-			fmt.Println()
+	// Register all commands
+	var completer = readline.NewPrefixCompleter()
+	children := []readline.PrefixCompleterInterface{}
+	methods := reflection.GetAllMethods(Dispatcher{})
+	for _, method := range methods {
+		if (strings.HasSuffix(method, "Help") && !strings.HasPrefix(method, "Help")) || strings.HasSuffix(method, "Desc") {
 			continue
 		}
-		input = strings.TrimSpace(input)
-		method, args := ParseInput(input)
+		children = append(children, readline.PcItem(method))
+	}
+	completer.SetChildren(children)
+
+	// Construct the IO
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          context.Ctx.CommandPrompt,
+		HistoryFile:     "/tmp/platypus.history",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+		HistorySearchFold:   true,
+		FuncFilterInputRune: filterInput,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	// Command loop
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+		method, args := parseInput(line)
 		if method == "" {
 			continue
 		}
---
+++ lib/util/fs/file.go
@@ -0,0 +1,15 @@
+package fs
+import (
+	"io/ioutil"
+)
+
+func listFiles(path string) func(string) []string {
+	return func(line string) []string {
+		names := make([]string, 0)
+		files, _ := ioutil.ReadDir(path)
+		for _, f := range files {
+			names = append(names, f.Name())
+		}
+		return names
+	}
+}
