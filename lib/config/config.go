6b27d859f40733ea7bd2d0305a7bff11153c5856
Wang Yihang <wangyihanger@gmail.com>
2018-09-04 13:22:00 +0800

    [+] More common

--- lib/config/config.go
+++ lib/config/config.go
@@ -20,15 +20,14 @@ func init(){
 }
 
 func printConfig(cfg *ini.File) {
-	fmt.Println("Flag config")
-	fmt.Println("\tCommand: ", cfg.Section("Flag").Key("Command").MustString("/bin/cat /flag"))
-	fmt.Println("Game config")
-	fmt.Println("\tRound: ", cfg.Section("Game").Key("Round").MustInt(300))
-	fmt.Println("Submit config")
-	fmt.Println("\tServer: ", cfg.Section("Submit").Key("Server").MustString("http://submit.flag.pwnable.cn"))
-	fmt.Println("\tCookie: ", cfg.Section("Submit").Key("Cookie").MustString("PHPSESSID=d72ja9fsjj2i20d0ashsahjdk21"))
-	fmt.Println("\tData: ", cfg.Section("Submit").Key("Data").MustString("flag=__FLAG__&token=1Y3nZoOMn66FBRTLYgd5LS52x1itXoJZIDawNtEvjh2PpdDFPtrH8adGXaAcjZuP"))
-	fmt.Println("\tAccept: ", cfg.Section("Submit").Key("Accept").MustString("accepted"))
+	fmt.Println("Batch config")
+	fmt.Println("\tCommand: ", cfg.Section("Batch").Key("Command").MustString("/bin/cat /flag"))
+	fmt.Println("\tRound: ", cfg.Section("Batch").Key("Round").MustInt(300))
+	fmt.Println("Report config")
+	fmt.Println("\tServer: ", cfg.Section("Report").Key("Server").MustString("http://report.pwnable.cn"))
+	fmt.Println("\tCookie: ", cfg.Section("Report").Key("Cookie").MustString("PHPSESSID=d72ja9fsjj2i20d0ashsahjdk21"))
+	fmt.Println("\tData: ", cfg.Section("Report").Key("Data").MustString("flag=__FLAG__&token=1Y3nZoOMn66FBRTLYgd5LS52x1itXoJZIDawNtEvjh2PpdDFPtrH8adGXaAcjZuP"))
+	fmt.Println("\tAccept: ", cfg.Section("Report").Key("Accept").MustString("accepted"))
 	fmt.Println("Crontab config")
 	fmt.Println("\tHost: ", cfg.Section("Crontab").Key("Host").MustString("1.3.3.7"))
 	fmt.Println("\tPort: ", cfg.Section("Crontab").Key("Port").MustInt(5555))
