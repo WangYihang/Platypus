41a45afef93ce58cae84dbe06c0977ba56fc7218
Wang Yihang <wangyihanger@gmail.com>
2018-09-04 13:16:39 +0800

    [+] Auto sync config file

--- platypus.go
+++ platypus.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"time"
 	"github.com/WangYihang/Platypus/lib/cli/dispatcher"
 	"github.com/WangYihang/Platypus/lib/context"
+	"github.com/WangYihang/Platypus/lib/config"  
 )
 
 func main() {
+	// Config loop
+	go func(){
+		for {
+			config.Cfg.Reload()
+			time.Sleep(time.Second * 3)
+		}
+	}()
 	// Create context
 	context.CreateContext()
 	// Run main loop
