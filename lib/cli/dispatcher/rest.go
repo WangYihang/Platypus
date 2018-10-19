cf36f9079ede64dd4bd8866c0e28cf405e9a502d
Wang Yihang <wangyihanger@gmail.com>
2018-10-19 22:28:19 +0800

    RESTful execute command

--- lib/cli/dispatcher/rest.go
+++ lib/cli/dispatcher/rest.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"io/ioutil"
 
+	"github.com/WangYihang/Platypus/lib/context"
 	"github.com/WangYihang/Platypus/lib/util/log"
 	"github.com/gin-gonic/gin"
 )
@@ -29,9 +30,34 @@ func (dispatcher Dispatcher) REST(args []string) {
 	gin.SetMode(gin.ReleaseMode)
 	gin.DefaultWriter = ioutil.Discard
 	rest := gin.Default()
-	rest.GET("/ping", func(c *gin.Context) {
+	rest.GET("/client", func(c *gin.Context) {
+		clients := []string{} 
+		for _, server := range context.Ctx.Servers {
+			for _, client := range (*server).GetAllTCPClients() {
+				clients = append(clients, client.Conn.RemoteAddr().String())
+			}
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"status": true,
+			"msg": clients,
+		})
+	})
+	rest.POST("/client/:hash", func(c *gin.Context) {
+		hash := c.Param("hash")
+		cmd := c.PostForm("cmd")
+		response := "No such client"
+		flag := false
+		for _, server := range context.Ctx.Servers {
+			for _, client := range (*server).GetAllTCPClients() {
+				if hash == client.Hash {
+					response =  client.SystemToken(cmd)
+					flag = true
+				}
+			}
+		}
+		c.JSON(200, gin.H{
+			"status": flag,
+			"msg": response,
 		})
 	})
 	go rest.Run(fmt.Sprintf("%s:%d", host, port))
