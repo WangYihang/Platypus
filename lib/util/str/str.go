10379293bfc961f4ef34fd76e3d2fffef88548da
Wang Yihang <wangyihanger@gmail.com>
2020-03-02 14:12:00 +0000

    [+] Add test files

--- lib/util/str/str.go
+++ lib/util/str/str.go
@@ -6,14 +6,17 @@ import (
 )
 
 func UpperCaseFirstChar(str string) string {
+	if len(str) == 0 {
+		return str
+	}
 	return strings.ToUpper(str[0:1]) + str[1:]
 }
 
 func RandomString(length int) string {
 	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    b := make([]byte, length)
-    for i := range b {
-        b[i] = charset[rand.Intn(len(charset))]
-    }
-    return string(b)
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
