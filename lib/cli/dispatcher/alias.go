3c725eeb46d0c96e8ddb0a901b0419772b47974f
Wang Yihang <wangyihanger@gmail.com>
2021-03-21 14:16:19 +0800

    [+] #12 Add alias command which gives a session a human readable name

---
+++ lib/cli/dispatcher/alias.go
@@ -0,0 +1,54 @@
+package dispatcher
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/WangYihang/Platypus/lib/context"
+	"github.com/WangYihang/Platypus/lib/util/log"
+	"github.com/fatih/color"
+)
+
+func (dispatcher Dispatcher) Alias(args []string) {
+	if len(args) != 1 {
+		log.Error("Arguments error, use `Help Alias` to get more information")
+		dispatcher.AliasHelp([]string{})
+		return
+	}
+
+	// Ensure the interactive session is set
+	context.Ctx.AllowInterrupt = true
+	if context.Ctx.Current == nil {
+		log.Error("Interactive session is not set, please use `Jump` command to set the interactive Interact")
+		return
+	}
+
+	// Alias session
+	log.Info("Renaming session: %s", context.Ctx.Current.FullDesc())
+	context.Ctx.Current.Alias = strings.TrimSpace(args[0])
+
+	// Update prompt
+	var user string
+	if context.Ctx.Current.User == "" {
+		user = "unknown"
+	} else {
+		user = context.Ctx.Current.User
+	}
+	ReadLineInstance.SetPrompt(color.CyanString(
+		"[%s] (%s) %s [%s] » ",
+		context.Ctx.Current.Alias,
+		context.Ctx.Current.OS.String(),
+		context.Ctx.Current.Conn.RemoteAddr().String(),
+		user,
+	))
+}
+
+func (dispatcher Dispatcher) AliasHelp(args []string) {
+	fmt.Println("Usage of Alias")
+	fmt.Println("\tAlias")
+}
+
+func (dispatcher Dispatcher) AliasDesc(args []string) {
+	fmt.Println("Alias")
+	fmt.Println("\tAlias the current session with a human-readable name.")
+}
