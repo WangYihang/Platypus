578e5797561206eeddfc2a399456855584bc607d
Wang Yihang <wangyihanger@gmail.com>
2021-05-02 17:04:07 +0800

    [+] Release v1.4.2

--- lib/util/update/update.go
+++ lib/util/update/update.go
@@ -10,7 +10,7 @@ import (
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
 
-const version = "1.4.1"
+const version = "1.4.2"
 
 func ConfirmAndSelfUpdate() {
 	log.Info("Detecting the latest version...")
