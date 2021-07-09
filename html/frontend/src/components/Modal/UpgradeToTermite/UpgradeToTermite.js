24cc6c5139db1c119782cb4bd4c9067d069ae09e
Wang Yihang <wangyihanger@gmail.com>
2021-07-09 22:03:56 +0800

    [*] Refact frontend: ClientsBody

---
+++ html/frontend/src/components/Modal/UpgradeToTermite/UpgradeToTermite.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Modal, Button, Select, Input } from "antd";
+
+const { Option } = Select;
+
+export default class UpgradeToTermite extends React.Component {
+    render() {
+        let upgradeButton;
+        if (this.props.line.CurrentProcessKey === undefined) {
+            upgradeButton = <Button disabled={false} onClick={() => { this.props.showModal() }}>Upgrade</Button>
+        } else {
+            upgradeButton = ""
+        }
+
+        return (
+            <>
+                <Button>
+                    <a
+                        href={this.props.baseUrl + "/shell/?" + this.props.line.hash}
+                        target={"_blank"}
+                        rel={"noreferrer noopener"}
+                    >
+                        Shell
+                    </a>
+                </Button>
+                {upgradeButton}
+                <Modal title="Basic Modal" visible={this.props.isModalVisible} onOk={() => this.props.handleOk(this.props.line.hash)} onCancel={() => this.props.handleCancel()}>
+                    Select Termite Listeners:
+                    <Select
+                        showSearch
+                        style={{ width: 200 }}
+                        placeholder="Select an termite listener"
+                        optionFilterProp="children"
+                        onChange={(value) => {
+                            this.props.setConnectBack(value)
+                        }}
+                        filterOption={(input, option) =>
+                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                        defaultValue={this.props.connectBack}
+                    >
+                        {this.props.serversList.map((entry) => {
+                            if (entry.encrypted) {
+                                return entry.interfaces.map((ifaddr) => {
+                                    let v = ifaddr + ":" + entry.port
+                                    return <Option value={v}>{v}</Option>
+                                })
+                            }
+                            return ""
+                        })}
+                    </Select>
+                    Input Termite Listeners Manually:
+                    <Input placeholder="1.3.3.7:13337" value={this.props.connectBack} onChange={(e) => {
+                        this.props.setConnectBack(e.target.value)
+                    }} />
+                </Modal>
+            </>
+        );
+    }
+}
+
+
+
