c3dcd96b817058a88fc9ed83aaa5429a282fe19c
Wang Yihang <wangyihanger@gmail.com>
2021-07-10 17:54:35 +0800

    [+] Add encrypted option to server creation (frontend)

--- html/frontend/src/components/Banner/ServerCreator/SeverCreator.js
+++ html/frontend/src/components/Banner/ServerCreator/SeverCreator.js
@@ -2,8 +2,20 @@ import React from "react";
 import PortSelector from "./PortSelector";
 import InterfaceSelector from "./InterfaceSelector";
 import CreateServerButton from "./CreateServerButton";
+import { Switch } from 'antd';
 
 export default class ServerCreator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            encrypted: true
+        };
+    }
+
+    changeEncryption(checked, event) {
+        this.setState({ encrypted: checked });
+    }
+
     render() {
         return <>
             <PortSelector
@@ -16,11 +28,13 @@ export default class ServerCreator extends React.Component {
                 serverCreatePort={this.props.serverCreatePort}
                 setServerCreateHost={this.props.setServerCreateHost}
             />
+            <Switch checkedChildren="Encrypted" unCheckedChildren="Plained" defaultChecked onChange={this.changeEncryption} />
             <CreateServerButton
                 apiUrl={this.props.apiUrl}
                 serverCreated={this.props.serverCreated}
                 serverCreateHost={this.props.serverCreateHost}
                 serverCreatePort={this.props.serverCreatePort}
+                serverCreateEncryption={this.state.encrypted}
                 serversList={this.props.serversList}
                 serversMap={this.props.serversMap}
                 setServerCreateHost={this.props.setServerCreateHost}
